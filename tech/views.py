a850dad7e594984dab31f1fc0c5a8d2901a2d27a
Adones Melo <adones.n.m@outlook.com>
2025-10-24 09:23:38 -0300

    Área para membros: criar uma página que será exibida caso o login seja feito

--- tech/views.py
+++ tech/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # função principal
 def index(request):
 	return render(request, 'index.html')
+
+def members(request):
+    return render(request, 'members.html')
